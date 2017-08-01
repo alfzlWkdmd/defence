5b6b7b311c82d7d1c0e38321a1d1d5b5001e1c5d
MILKI <dudgns2317@gmail.com>
2017-08-01 17:22:03 +0900

    마무리완성

--- Assets/WayPoint.cs
+++ Assets/WayPoint.cs
@@ -6,12 +6,13 @@ public class WayPoint : MonoBehaviour {
 
     public static Transform[] point;
 
-    void awake()
+    void Awake()
     {
         point = new Transform[transform.childCount];
         for (int i=0;i<point.Length;i++)
         {
             point[i] = transform.GetChild(i);
+            Debug.Log(point[i]);
         }
     }
 }
--- Assets/enemymove.cs
+++ Assets/enemymove.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class enemymove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed = 3.0f;
+
+    private Transform target;
+    private int wavepointindex = 0;
+    void Start()
+    {
+        target = WayPoint.point[0];
+    }
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime,Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            getnextwaypoint();
+        }
+    }
+
+    void getnextwaypoint()
+    {
+        if(wavepointindex>=WayPoint.point.Length-1)
+        {
+            wavepointindex = 0;
+            target = WayPoint.point[wavepointindex];
+            return;
+        }
+        wavepointindex++;
+        target = WayPoint.point[wavepointindex];
+    }
 }
