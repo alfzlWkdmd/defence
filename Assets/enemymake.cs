912baf54831c179c358fb166a4dab71be6dd5a83
MILKI <dudgns2317@gmail.com>
2017-08-02 15:38:07 +0900

    카운트10이후게임정지

--- Assets/enemymake.cs
+++ Assets/enemymake.cs
@@ -3,19 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class enemymake : MonoBehaviour {
-
+    private int i;
     public GameObject Enemy;
     void SpawnEnemy()
     {
         Instantiate(Enemy, transform.position, Quaternion.identity);
     }
+    void Count()
+    {
+        i = i + 1;
+    }
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("SpawnEnemy", 1, 1);
+        InvokeRepeating("Count", 1, 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+        if (i==10)
+        {
+            Time.timeScale=0.0f;
+        }
+
     }
 }
