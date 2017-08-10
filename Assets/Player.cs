1c76a71e27d7c59126ea5ff9948086cf6077a6ed
MILKI <dudgns2317@gmail.com>
2017-08-10 22:36:56 +0900

    플래이어스크립트생성

---
+++ Assets/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int MyLife = 10;
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
