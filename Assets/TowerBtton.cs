dfbf06fe237fea707e30323beed796a4f02af398
MILKI <dudgns2317@gmail.com>
2017-08-14 21:37:43 +0900

    마지막적부터죽이는버튼스크립트작성완료

--- Assets/TowerBtton.cs
+++ Assets/TowerBtton.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerBtton : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Player player;
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+	public void KillLastEnemy()
+    {
+        if(EnemyList.targetlist.Count<=0)
+        {
+            return;
+        }
+        Destroy(EnemyList.targetlist[0].gameObject);
+        EnemyList.targetlist.RemoveAt(0);
+        player.MyLife++;
+    }
 }
