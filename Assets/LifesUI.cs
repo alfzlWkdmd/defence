7df303fd7d24dfa7facf6fb641d735023e6d1420
MILKI <dudgns2317@gmail.com>
2017-08-16 21:08:37 +0900

    플레이어생명력텍스트생성&완성

---
+++ Assets/LifesUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifesUI : MonoBehaviour {
+   
+    public Text LifesText;
+    void Start()
+    {
+     
+    }
+	
+	void Update () {
+        LifesText.text = Player.Life.ToString()+"Lifes";
+		}
+}
